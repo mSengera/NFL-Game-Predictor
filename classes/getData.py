e31854608121b0dc0f1d4a91823a6823874775ee
mSengera <work@marvin-sengera.de>
2018-11-20 17:21:39 +0100

    Change getData.py

--- classes/getData.py
+++ classes/getData.py
@@ -1,4 +1,15 @@
+import nflgame
+
+
 class GetData:
 
+    nfl_years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
+
     def get_rushing_yards_per_team(self, home, away):
+
+        for year in self.nfl_years:
+            games = nflgame.games(year)
+
+            print(year)
+
         return []
