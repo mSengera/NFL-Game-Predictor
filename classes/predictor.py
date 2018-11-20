f81c66431eee6eaf1c797922482dc3e0381f86f3
mSengera <work@marvin-sengera.de>
2018-11-20 18:21:41 +0100

    Change predictor class

--- classes/predictor.py
+++ classes/predictor.py
@@ -19,29 +19,8 @@ class Predictor:
     def predict(self):
         print('I will predict... ' + str(self.first_team) + "/" + str(self.second_team))
 
-        """
-        Start by predicting the rushing yards per team
-        """
-        path = "classes/regression_functions/predict_rushing_yards/" + str(self.first_team) + "-" + str(self.second_team) + ".json"
-        data_function = Path(path)
-
-        if data_function.is_file():
-            print("Load the data.")
-
-            file = open(path, "r")
-            json_string = file.readline()
-
-            function_parameter = json.loads(json_string)
-        else:
-            print("Generate the data.")
-            f = CalculateRegressionFunction()
-            function_parameter = f.calculate_average_rushing_yards(self.first_team, self.second_team)
-
-            file = open(path, "w")
-            file.write(json.dumps(function_parameter))
-
-        self.rushing_yards_home = function_parameter[0]
-        self.rushing_yards_away = function_parameter[1]
+        # Predict rushing yards per team
+        self.predict_rushing_yards()
 
         """
         Predicting the passing yards per team
@@ -52,3 +31,25 @@ class Predictor:
         """
 
         exit()
+
+    def predict_rushing_yards(self):
+        path = "classes/regression_functions/predict_rushing_yards/" + str(self.first_team) + "-" + str(self.second_team) + ".json"
+        data_function = Path(path)
+
+        if data_function.is_file():
+            print("Load the data for rushing yards.")
+
+            file = open(path, "r")
+            json_string = file.readline()
+
+            function_parameter = json.loads(json_string)
+        else:
+            print("Generate the data for rushing yards.")
+            f = CalculateRegressionFunction()
+            function_parameter = f.calculate_average_rushing_yards(self.first_team, self.second_team)
+
+            file = open(path, "w")
+            file.write(json.dumps(function_parameter))
+
+        self.rushing_yards_home = function_parameter[0]
+        self.rushing_yards_away = function_parameter[1]
